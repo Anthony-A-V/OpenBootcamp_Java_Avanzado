d79a68c0891063567a28ff24b1ffdf79299d4e3c
AnthonyANVA <111375444+AnthonyANVA@users.noreply.github.com>
2022-12-20 10:47:23 -0500

    changes

---
+++ Ejercicios_tema_12_13_14/src/Main.java
@@ -0,0 +1,38 @@
+import javax.management.AttributeList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    /**
+     * Variable lista del Tipo Integer que se usará para agregar e iterar sus valores
+     */
+    public static List<Integer> listaEnteros = new ArrayList<>();
+
+    /**
+     * Función principal de la clase Main
+     * @param args Valores de los argumentos
+     */
+    public static void main(String[] args) {
+        agregarLista(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        iterarLista();
+    }
+
+    /**
+     * Función que agrega una cantidad variable de valores Integer a la lista listaEnteros
+     * @param enteros Valores Integers que se le agrega a la lista
+     */
+    public static void agregarLista(Integer... enteros) {
+        listaEnteros.addAll(Arrays.asList(enteros));
+    }
+
+    /**
+     * Función que itera la lista listaEnteros e imprime sus valores
+     */
+    public static void iterarLista() {
+        for (int entero : listaEnteros) {
+            System.out.println(entero);
+        }
+    }
+}
